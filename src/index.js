cf6a559832f322172e6a5a460e97ab3961b2c60d
jbrandinger <98676769+jbrandinger@users.noreply.github.com>
2024-08-18 18:08:56 -0400

    add react functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>How are you doing today?</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,25 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,  // Process both .js and .jsx files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],  // Allow imports without needing extensions
+  },
   devServer: {
     static: './dist',
   },
   mode: 'development',
 };
-
-
